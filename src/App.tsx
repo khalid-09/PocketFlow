bb766c9b9390d992622105c0a1764aa472223faa
Khalid Ansari <khaliljpc@gmail.com>
2024-01-17 19:13:18 +0530

    Added shadcn UI lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from './components/ui/button';
+
 const App = () => {
-  return <div className="font-bold underline text-red-600">App</div>;
+  return (
+    <div className="font-bold underline text-red-600">
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
 };
 
 export default App;
