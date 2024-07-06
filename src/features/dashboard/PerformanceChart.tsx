927ca71e9ecaf64a3c485827010dac5c19a15069
Khalid Ansari <khaliljpc@gmail.com>
2024-07-06 11:31:34 +0530

    feat: added shadcn charts for performance chart in dashboard

--- src/features/dashboard/PerformanceChart.tsx
+++ src/features/dashboard/PerformanceChart.tsx
@@ -2,6 +2,8 @@ import { DateRange } from 'react-day-picker';
 import { usePerformace } from './usePerformace';
 import { useMemo } from 'react';
 import { formatDate } from '@/utils/helpers';
+
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts';
 import {
   Card,
   CardContent,
@@ -10,26 +12,40 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import {
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-  BarChart,
-  Legend,
-  Bar,
-} from 'recharts';
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart';
 
-export function PerformanceChart({ date }: { date: DateRange | undefined }) {
+const chartConfig = {
+  expenses: {
+    label: 'Expenses',
+    color: 'hsl(var(--chart-1))',
+  },
+  incomes: {
+    label: 'Income',
+    color: 'hsl(var(--chart-2))',
+  },
+} satisfies ChartConfig;
+
+export function PerformanceBarChart({ date }: { date: DateRange | undefined }) {
   const { data } = usePerformace(date);
-  console.log(data);
 
   const sortedData = useMemo(() => {
-    return data?.sort(
-      (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
-    );
+    if (data) {
+      return data
+        .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+        .map(item => ({
+          ...item,
+          incomes: item.incomes ?? 0,
+          expenses: item.expenses ?? 0,
+        }));
+    }
+    return [];
   }, [data]);
-  console.log(sortedData);
+
+  console.log('Sorted Data:', sortedData);
 
   const from = date?.from ? `From ${formatDate(date?.from, 'medium')}` : '';
   const to = date?.to ? `To ${formatDate(date?.to, 'medium')}` : '';
@@ -41,19 +57,46 @@ export function PerformanceChart({ date }: { date: DateRange | undefined }) {
         <CardDescription>Expense / Income</CardDescription>
       </CardHeader>
       <CardContent>
-        <ResponsiveContainer height={300} width="100%">
-          <BarChart data={sortedData ?? []}>
-            <CartesianGrid strokeDasharray="4" />
-            <XAxis dataKey="date" />
-            <YAxis />
-            <Tooltip contentStyle={{ backgroundColor: '#aaa' }} />
-            <Legend />
-            <Bar dataKey="expenses" name="Expense" fill="#ef4444" />
-            <Bar dataKey="incomes" name="Income" fill="#4ade80" />
-          </BarChart>
-        </ResponsiveContainer>
+        {sortedData.length === 0 ? (
+          <div className="flex items-center justify-center h-48">
+            <span>No data to display</span>
+          </div>
+        ) : (
+          <ChartContainer config={chartConfig}>
+            <BarChart
+              accessibilityLayer
+              data={sortedData}
+              margin={{
+                left: -20,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="date"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={value => {
+                  const date = new Date(value);
+                  return date.toLocaleDateString('en-US', { month: 'short' });
+                }}
+              />
+              <YAxis
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickCount={3}
+              />
+              <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+              <Bar dataKey="expenses" fill="var(--color-expenses)" radius={4} />
+              <Bar dataKey="incomes" fill="var(--color-incomes)" radius={4} />
+            </BarChart>
+          </ChartContainer>
+        )}
       </CardContent>
     </Card>
   );
 }
-export default PerformanceChart;
+
+export default PerformanceBarChart;
